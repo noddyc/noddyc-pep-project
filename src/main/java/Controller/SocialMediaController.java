6e9d9b6c6b5e26814cf33d5509781cd7752a2dff
Jian He <hej02184@gmail.com>
2025-04-17 04:24:31 +0000

    initial2

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -37,7 +37,7 @@ public class SocialMediaController {
     }
 
     private void registerHandler(Context context){
-        
+        context.json('')
     }
 
 
---
+++ src/main/java/Service/AccountService.java
@@ -0,0 +1,20 @@
+package Service;
+
+import DAO.AccountDAO;
+import Model.Account;
+
+public class AccountService {
+    private AccountDAO accountDAO;
+
+    public AccountService(){
+        this.accountDAO = new AccountDAO();
+    }
+
+    public AccountService(AccountDAO accountDAO){
+        this.accountDAO = accountDAO;
+    }
+
+    public Account register(Account account){
+        return accountDAO.insertAccount(account);
+    }
+}
